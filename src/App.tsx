0f34860c3d2f04b6ba19d9ce61785d437d5301fe
Erlan-bam <erlanzh.gg@gmail.com>
2024-08-04 15:27:41 +0500

    --ALL THINGS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
 import { useEffect } from "react";
-import { CustomColorMenu, TextMenu, Wrapper } from "spark-admin-sdk";
+import {
+  CustomColorMenu,
+  TextMenu,
+  Wrapper,
+  LinkMenu,
+  BlockEditMenu,
+  IconLinkMenu,
+  ButtonMenu,
+} from "spark-admin-sdk";
 import { ColorMenu } from "spark-admin-sdk";
 
 function App() {
   useEffect(() => {
     console.log(window.getSelection());
   });
+  const section = [
+    {
+      name: "Section1",
+      anchor: "#anchor1",
+    },
+    {
+      name: "Section2",
+      anchor: "#anchor2",
+    },
+  ];
+  const page = [
+    {
+      url: "http://localhost:5173/",
+      name: "test-sdk",
+    },
+    {
+      url: "http://localhost:5173/",
+      name: "test-sdk",
+    },
+  ];
 
   return (
     <>
@@ -26,6 +54,14 @@ function App() {
       <TextMenu />
       <ColorMenu />
       <CustomColorMenu />
+      <LinkMenu
+        sectionOptions={section}
+        pageOptions={page}
+        onChange={(value: string) => console.log(value)}
+      />
+      <IconLinkMenu onClick={() => console.log("works")} />
+      <BlockEditMenu onClick={() => console.log("works")} isLoading={false} />
+      <ButtonMenu onClick={() => console.log("works")} />
     </>
   );
 }
